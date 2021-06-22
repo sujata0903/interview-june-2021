d1c1af29f01099fe18d2f771a6fabe06ba508576
sujata0903 <86252366+sujata0903@users.noreply.github.com>
2021-06-22 13:14:48 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -2,9 +2,33 @@ package exam;
 
 public class ShiftElementsInArray {
 
-  public void shift(int[] arr) {
+  static void shift(int[] arr) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
 
+         int l = arr[arr.length - 1];
+        for (int i = arr.length - 2; i >= 0; i--) 
+        {
+            arr[i + 1] = arr[i];
+        }
+
+        arr[0] = l;
+       }
+public static void rightRotate(int[] arr, int a)
+    {
+        for (int i = 0; i < a; i++) 
+         {
+            shift(arr);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int a = 1;
+
+        rightRotate(arr, a);
+
+        System.out.println(Arrays.toString(arr));
   }
 
 }
